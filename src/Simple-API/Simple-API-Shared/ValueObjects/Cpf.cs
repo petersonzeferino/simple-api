3f0612bbf0dc26ed487412c2aaa49c5e47f7cafe
Peterson <petersonzeferino@hotmail.com>
2021-10-13 17:49:49 -0300

    Create Empty Value Object for Cpf

---
+++ src/Simple-API/Simple-API-Shared/ValueObjects/Cpf.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Simple_API_Shared.ValueObjects
+{
+    public struct Cpf
+    {
+        //private readonly string _value;
+
+        //private Cpf(string _value) { _value = value; }
+
+        //public static Cpf Parse(string value)
+        //{
+        //    if (TryParse(value, out var result))
+        //    {
+        //        return result;
+        //    }
+        //    throw new ArgumentException(...);
+        //}
+
+        //public static bool TryParse(string value, out Cpf cpf)
+        //{
+        //    //.. validation Logic 
+        //}
+
+        //public static implicit operator Cpf(string value)
+        //  => Parse(value);
+    }
+}
