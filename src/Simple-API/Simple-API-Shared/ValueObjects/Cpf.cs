579061bd8f2a859cf2fd7fc4e4522097a1948162
Peterson <petersonzeferino@hotmail.com>
2021-10-14 10:58:36 -0300

    Include logic to Cpf

--- src/Simple-API/Simple-API-Shared/ValueObjects/Cpf.cs
+++ src/Simple-API/Simple-API-Shared/ValueObjects/Cpf.cs
@@ -4,25 +4,34 @@ namespace Simple_API_Shared.ValueObjects
 {
     public struct Cpf
     {
-        //private readonly string _value;
+        private readonly string _value;
 
-        //private Cpf(string _value) { _value = value; }
+        private Cpf(string value)
+        {
+            _value = value;
+        }
 
-        //public static Cpf Parse(string value)
-        //{
-        //    if (TryParse(value, out var result))
-        //    {
-        //        return result;
-        //    }
-        //    throw new ArgumentException(...);
-        //}
+        public override string ToString()
+            => _value;
 
-        //public static bool TryParse(string value, out Cpf cpf)
-        //{
-        //    //.. validation Logic 
-        //}
+        public static implicit operator Cpf(string input)
+            => Parse(input);
 
-        //public static implicit operator Cpf(string value)
-        //  => Parse(value);
+        public static Cpf Parse(string value)
+        {
+            if (TryParse(value, out var result))
+            {
+                return result;
+            }
+            
+            throw new ArgumentException("Cpf is not valid");
+        }
+
+        public static bool TryParse(string value, out Cpf cpf)
+        {
+            //TODO: Include validation 
+            cpf = new Cpf(value);
+            return true;
+        }
     }
 }
